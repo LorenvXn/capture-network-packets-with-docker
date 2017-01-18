43c7332630ff2770dc69149b04219f4d680e8d9a
Satanette <predark.dante@gmail.com>
2017-01-18 12:24:38 +0200

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -2,12 +2,13 @@
 
  docker build -t sniffit1 .
 
-
+echo
 echo "###########Sniffing interface from container################ "
 echo
-## while sleep 1; do docker run -ti -p 8080:8080 sniffit1 ./handle /bin/bash ; done; #where is mah mind?!...
-
+## while sleep 1; do docker run -ti -p 8080:8080 sniffit1 ./blablascript /bin/bash ; done; #where is mah mind?!...
 docker run -ti sniffit1 ./find_device /bin/bash
-
+echo
+echo "############Packet capture length############################"
+echo
 docker run -ti sniffit1 ./packet_length /bin/bash
  
