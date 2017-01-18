6d619a6d24acbb6cfd824995227242481910fdc2
Satanette <predark.dante@gmail.com>
2017-01-18 12:17:19 +0200

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -5,4 +5,9 @@
 
 echo "###########Sniffing interface from container################ "
 echo
- while sleep 1; do docker run -ti -p 8080:8080 sniffit1 ./handle /bin/bash ; done; #where is mah mind?!...
+## while sleep 1; do docker run -ti -p 8080:8080 sniffit1 ./handle /bin/bash ; done; #where is mah mind?!...
+
+docker run -ti sniffit1 ./find_device /bin/bash
+
+docker run -ti sniffit1 ./packet_length /bin/bash
+ 
